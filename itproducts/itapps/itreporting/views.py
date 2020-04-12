e9a05c4bc102bc18ef4caaf8ea828dc10aa8ebce
user <user@b6005208.localdomain>
2020-04-12 22:55:58 +0100

    Created some of the pages and content

--- itproducts/itapps/itreporting/views.py
+++ itproducts/itapps/itreporting/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return render(request, 'itreporting/home.html')
+def about(request):
+   return render(request, 'itreporting/about.html')
+def contact(request):
+    return render(request, 'itreporting/contact.html')
+def products(request):
+    return render(request, 'itreporting/products.html')
+def smartphone(request):
+    return render(request, 'itreporting/smartphone.html')
+def smartwatch(request):
+    return render(request, 'itreporting/smartwatch.html')
+def smarttv(request):
+    return render(request, 'itreporting/smarttv.html')
