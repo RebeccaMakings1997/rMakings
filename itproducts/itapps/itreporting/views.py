c58322f5442eea982ad77da1ca95efda15085bb4
user <user@b6005208.localdomain>
2020-04-13 20:56:43 +0100

    Created navigation and CSS for the pages

--- itproducts/itapps/itreporting/views.py
+++ itproducts/itapps/itreporting/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'itreporting/home.html')
+    return render(request, 'itproducts/home.html')
 def about(request):
-   return render(request, 'itreporting/about.html')
+   return render(request, 'itproducts/about.html',{'title': 'About us'})
 def contact(request):
-    return render(request, 'itreporting/contact.html')
+    return render(request, 'itproducts/contact.html')
 def products(request):
-    return render(request, 'itreporting/products.html')
+    return render(request, 'itproducts/products.html')
 def smartphone(request):
-    return render(request, 'itreporting/smartphone.html')
+    return render(request, 'itproducts/smartphone.html')
 def smartwatches(request):
-    return render(request, 'itreporting/smartwatches.html')
+    return render(request, 'itproducts/smartwatches.html')
 def smarttv(request):
-    return render(request, 'itreporting/smarttv.html')
+    return render(request, 'itproducts/smarttv.html')
+def smarttv(request):
+    return render(request, 'itproducts/smarttv.html')
+def comment(request):
+    return render(request, 'itproducts/comment.html')
